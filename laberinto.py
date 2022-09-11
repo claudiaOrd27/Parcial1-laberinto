5922dd47abe3a188b750635b1a7eb6365dc057bf
don17299 <carlosm.duquem@uqvirtual.edu.co>
2022-09-11 17:12:40 -0500

    niveles y movimientos

--- laberinto.py
+++ laberinto.py
@@ -32,11 +32,21 @@ class Player(turtle.Turtle):
         self.hideturtle()
         self.penup()
         self.speed(0)
+
+class Texto(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("arrow")
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.speed(0)
 #-------------------------------------------
 #Creación del laberinto.
 maze = Maze()
 wall_color= Color()
 player=Player()
+texto_general=Texto()
 
 walls = []
 path = []
@@ -70,22 +80,32 @@ lab = [
 
 ]
 
+contadorPlayer=0
+contadorIA=0
+nivel=1
+
 #-------------------------------------------
 #creación de la ventana con turtle
-def principal(wn):
-    wn.clear()
-    wn.setup(600,600)
-    wn.bgpic("files/maze_image1.gif")
+def principal(ventana):
+    ventana.clear()
+    ventana.setup(600,600)
+    ventana.bgcolor("Black")
+    ventana.bgpic("files/maze_image1.gif")
     texto=turtle.Turtle()
     texto.hideturtle()
     texto.penup()
-    texto.goto(-290,130)
+    texto.goto(-290,200)
     texto.color('white')
     style =('Courier', 20, 'italic', 'bold')
     texto.write('Laberinto de Inteligencia Artificial', font=style, move=True)
-
     makeButton(80,-150,-200,"Jugar")
     makeButton(175,50,-200,"Hacerlo con IA")
+    texto_general.goto(-290,150)
+    global nivel
+    nivel=1
+    texto_general.write('Nivel actual: 1', font=('Courier', 12, 'italic', 'bold'), move=True)
+    makeButton(200,-50, 150,"Cambiar nivel")
+    
 
     turtle.onscreenclick(buttonsMainViewClick,1)
     turtle.listen()
@@ -111,6 +131,72 @@ def makeButton(tam,x,y,msg):
     penButtons.goto(x+6,y+7)
     penButtons.write(msg, font=["Courier",15,"bold"])
 
+def cambiarNivel():
+        global lab
+        if nivel==1:
+            lab = [
+            "XXXXXXXXXXXXXXXXXXXXXXX",
+            "X               X     X",
+            "XS XXXXX XXXX  XXXXX  X",
+            "X          X          X",
+            "X  XXX XXX  XXXXX  XXXX",
+            "X  X     X     X  X   X",
+            "X  X X X X  X  X  X   X",
+            "X  X  X  X  X      X  X",
+            "X  X    X  XXXXXX  X  X",
+            "X  X        X      X  X",
+            "X  XXXX  X  XXXXXXXEX X",
+            "X     X  X     X      X",
+            "X XX  X  X  XXXXX XXXXX",
+            "X  X  X               X",
+            "X  X   XXX  XXXXX  X  X",
+            "X  X  X     X      X  X",
+            "X  X    XX XX  X  X  XX",
+            "XXXXXXXXXXXXXXXXXXXXXXX",
+            ]
+        elif nivel == 2:
+            lab = [
+            "XXXXXXXXXXXXXXXXXXXXXXX",
+            "X          XXXXXXXXX  E",
+            "X XXXX XX            XX",
+            "X XXXX XXXXXXXXXXXXX XX",
+            "X             XXXXXX XX",
+            "XXXXXXXXX XXX XXXXXX XX",
+            "XXXXXXXXX XXX X      XX",
+            "X         XXXXXXXXXXXXX",
+            "X XXXXXXXXX          XX",
+            "X X         XX  X XXXXX",
+            "X XXXXXXXXXXXX  XXXXXXX",
+            "X    X XXXX        XXXX",
+            "XXXX        XXXXXX XXXX",
+            "XXXX XXXXXX XX     XXXX",
+            "X           XX XXXXXXXX",
+            "X XXXXXXXXX XX      XXX",
+            "X         X XXXXXXXXXXX",
+            "XXXXXXXXXXXSXXXXXXXXXXX",
+            ]
+        else:
+            lab = [
+            "XXXXXXXXXXXXXXXXXXXXXXX",
+            "X  X            X     X",
+            "X  XXXXX XXXX  XXXXX  X",
+            "X          X          X",
+            "X  XXX XXX XXXXXX  XXXX",
+            "X  X     X  X  X  X   X",
+            "X  X     X  X  X  X   X",
+            "X  X  XXXX  X      X  X",
+            "X  X    X  XXXXXX  X  X",
+            "X          SX      X  X",
+            "X  XXXXXXX  XXXX XXXX X",
+            "X     X  X            X",
+            "X XX  X  XXXXXXXX XXXXX",
+            "X  X  X               X",
+            "X  X  XXXX  XXXXX  X  X",
+            "X  XXXX     X      X  X",
+            "XE      XXXXX  X  XX  X",
+            "XXXXXXXXXXXXXXXXXXXXXXX",
+            ]
+
 
 #Evento para los botones de la primera pantalla
 def buttonsMainViewClick(x,y):
@@ -134,6 +220,10 @@ def buttonsMainViewClick(x,y):
         texto.color("red")
         texto.goto(-280, 250)
         texto.stamp()
+        texto.color("purple")
+        texto.shape('circle')
+        texto.goto(30, 250)
+        texto.stamp()
 
         texto.goto(-260,240)
         texto.color('white')
@@ -145,6 +235,11 @@ def buttonsMainViewClick(x,y):
         style =('Courier', 15 , 'italic', 'bold')
         texto.write(': Salida.', font=style, move=True)
 
+        texto.goto(50,240)
+        texto.color('white')
+        style =('Courier', 15 , 'italic', 'bold')
+        texto.write(': Jugador.', font=style, move=True)
+
     #Resolver con IA
     if x> 40 and x< 190 and y> -200 and y< -170:
         wn.clear()
@@ -154,17 +249,47 @@ def buttonsMainViewClick(x,y):
         
         turtle.onscreenclick(buttonsMazeClick,1)
         turtle.listen()
+
+    #cambiar nivel
+    if x> -50 and x< 150 and y> 150 and y< 180:
+        global nivel
+        texto_general.goto(-290,150)
+        style =('Courier', 12 , 'italic', 'bold')
+        
+        if nivel==1:
+            nivel=2
+            texto_general.clear()
+            texto_general.write('Nivel actual: 2', font=style, move=True)
+            cambiarNivel()
+        elif nivel==2:
+            nivel=3
+            texto_general.clear()
+            texto_general.write('Nivel actual: 3', font=style, move=True)
+            cambiarNivel()
+        else:
+            nivel=1
+            texto_general.clear()
+            texto_general.write('Nivel actual: 1', font=style, move=True)
+            cambiarNivel()
     
         
 #Evento para los botones de la segunda pantalla
 def buttonsMazeClick(x,y):
     if x> -50 and x< 60 and y> 210 and y< 240:
         #------------------------------------------
-        #Usando el algoritmo Breath first para encontrar la ruta mas corta.
+        #Usando el algoritmo Breath first search para encontrar la ruta mas corta.
 
         buscar(start_x,start_y)
         camino_mas_corto(end_x, end_y)
         makeButton(110,-50,210,"Volver")
+        texto= turtle.Turtle()
+        texto.hideturtle()
+        texto.penup()
+        texto.goto(-250,260)
+        texto.color('white')
+        style =('Courier', 15 , 'italic', 'bold')
+        msg='El camino mas corto es de '+ str(contadorIA) +" movimientos"
+        texto.write(msg, font=style, move=True)
         turtle.onscreenclick(volverI)
 
 #Función para salir del programa.
@@ -179,6 +304,11 @@ def volver(x,y):
 
 def inicializar_laberinto(lab):
     global start_x, start_y, end_x, end_y
+    walls.clear()
+    path.clear()
+    visited.clear()
+    frontier.clear()
+    solution.clear()
     
     for y in range(len(lab)):
         for x in range(len(lab[y])):
@@ -212,6 +342,8 @@ def up():
         player.clear()
         player.goto(x,y)
         player.stamp()
+        global contadorPlayer
+        contadorPlayer+=1
     if (x,y) == (fin_x,fin_y):
         time.sleep(0.5)
         wn.clear()
@@ -219,6 +351,14 @@ def up():
         wn.setup(500,500)
         wn.bgpic("files/di.gif")
         makeButton(110,-200,-180,"Volver")
+        texto= turtle.Turtle()
+        texto.hideturtle()
+        texto.penup()
+        texto.goto(-200,200)
+        texto.color('white')
+        style =('Courier', 15 , 'italic', 'bold')
+        msg='Bien!, lo hiciste en '+ str(contadorPlayer) +" movimientos"
+        texto.write(msg, font=style, move=True)
         turtle.onscreenclick(volver)
 
 
@@ -228,6 +368,8 @@ def left():
         player.clear()
         player.goto(x,y)
         player.stamp()
+        global contadorPlayer
+        contadorPlayer+=1
     if (x,y) == (fin_x,fin_y):
         time.sleep(0.5)
         wn.clear()
@@ -235,6 +377,14 @@ def left():
         wn.setup(500,500)
         wn.bgpic("files/di.gif")
         makeButton(110,-200,-180,"Volver")
+        texto= turtle.Turtle()
+        texto.hideturtle()
+        texto.penup()
+        texto.goto(-200,200)
+        texto.color('white')
+        style =('Courier', 15 , 'italic', 'bold')
+        msg='Bien!, lo hiciste en '+ str(contadorPlayer) +" movimientos"
+        texto.write(msg, font=style, move=True)
         turtle.onscreenclick(volver)
 
 
@@ -244,12 +394,23 @@ def right():
         player.clear()
         player.goto(x,y)
         player.stamp()
+        global contadorPlayer
+        contadorPlayer+=1
     if (x,y) == (fin_x,fin_y):
         time.sleep(0.5)
         wn.clear()
+        wn.bgcolor("Black")
         wn.setup(500,500)
         wn.bgpic("files/di.gif")
         makeButton(110,-200,-180,"Volver")
+        texto= turtle.Turtle()
+        texto.hideturtle()
+        texto.penup()
+        texto.goto(-200,200)
+        texto.color('white')
+        style =('Courier', 15 , 'italic', 'bold')
+        msg='Bien!, lo hiciste en '+ str(contadorPlayer) +" movimientos"
+        texto.write(msg, font=style, move=True)
         turtle.onscreenclick(volver)
 
 
@@ -259,21 +420,34 @@ def down():
         player.clear()
         player.goto(x,y)
         player.stamp()
+        global contadorPlayer
+        contadorPlayer+=1
     if (x,y) == (fin_x,fin_y):
         time.sleep(0.5)
         wn.clear()
+        wn.bgcolor("Black")
         wn.setup(500,500)
         wn.bgpic("files/di.gif")
         makeButton(110,-200,-180,"Volver")
+        texto= turtle.Turtle()
+        texto.hideturtle()
+        texto.penup()
+        texto.goto(-200,200)
+        texto.color('white')
+        style =('Courier', 15 , 'italic', 'bold')
+        msg='Bien!, lo hiciste en '+ str(contadorPlayer) +" movimientos"
+        texto.write(msg, font=style, move=True)
         turtle.onscreenclick(volver)
 
 #Keyboard Binds
 def inicializar_controles(x,y,e_x,e_y):
-    global fin_x,fin_y
+    global fin_x,fin_y, contadorPlayer
+    contadorPlayer=0
     fin_x=e_x
     fin_y=e_y
     player.goto(x,y)
     player.color("purple")
+    player.stamp()
     wn.listen()
     wn.onkeypress(up,"Up")
     wn.onkeypress(left,"Left")
@@ -329,12 +503,16 @@ def buscar(x,y):
         wall_color.stamp()
 
 def camino_mas_corto(x, y):
+    global contadorIA
+    contadorIA=0
     wall_color.color('chartreuse')
     wall_color.goto(x, y)
     wall_color.stamp()
+
     while (x, y) != (start_x, start_y):
         wall_color.goto(solution[x, y])
         wall_color.stamp()
+        contadorIA+=1
         x, y = solution[x, y]
 
 
